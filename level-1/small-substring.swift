c966c436e155fea53602058d80c7d27f0cf084bf
yooouri <62045427+yooouri@users.noreply.github.com>
2023-01-08 21:39:02 +0900

    Update small-substring.swift

--- level-1/small-substring.swift
+++ level-1/small-substring.swift
@@ -4,24 +4,21 @@ func solution(_ t:String, _ p:String) -> Int {
     var tLen = t.count
     var pLen = p.count
     var answer = 0
-//     str[str.startIndex ..< str.endIndex]
-// let nums: [Int] = [1, 2, 3, 4]
- var tmp: [String] = []
+    var tmp: [String] = []
     // print(tLen-pLen+1)
-for (i, ele) in t.enumerated() {
-    if(i==(tLen-pLen+1)) {break;}
-   let startIndex = t.index(t.startIndex, offsetBy: i)// 사용자지정 시작인덱스
-let endIndex = t.index(t.startIndex, offsetBy: i+pLen)// 사용자지정 끝인덱스
-var sliced_str = t[startIndex ..< endIndex]
-// print(type(of:sliced_str))
-    tmp.append(String(sliced_str));
-}
+    for (i, ele) in t.enumerated() {
+        if(i==(tLen-pLen+1)) {break;}
+            let startIndex = t.index(t.startIndex, offsetBy: i)// 사용자지정 시작인덱스
+            let endIndex = t.index(t.startIndex, offsetBy: i+pLen)// 사용자지정 끝인덱스
+            var sliced_str = t[startIndex ..< endIndex]
+            // print(type(of:sliced_str))
+            tmp.append(String(sliced_str));
+     }
     
     for ele in tmp {
         guard let ele = Int(ele), let p = Int(p) else {fatalError()}
         if Int(ele) <= Int(p) { answer += 1}
     }
-
     
     return answer
 }
